12c9e9f4fc0bf4c708c5258465dc106eb89da429
Vinicius de Souto <viniciusdesouto@gmail.com>
2024-06-30 16:39:58 -0300

    header finish

---
+++ src/components/Header/styles.ts
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+import coverImg from "../../assets/cover.svg"
+
+export const HeaderContainer = styled.header`
+  width: 100%;
+  height: 18.5rem;
+  background-image: url(${coverImg});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
